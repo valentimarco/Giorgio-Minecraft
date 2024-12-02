4d902bfba03e874c5fe64989210e90d89b86c711
valentimarco <gamespy67@gmail.com>
2024-12-02 01:21:33 +0100

    feat: init frontend - Add colmuns and navbar

---
+++ frontend/app/root.tsx
@@ -0,0 +1,56 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import "./tailwind.css";
+
+export const links: LinksFunction = () => [
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
+];
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="grid grid-cols-[256px_1fr] gap-1">
+        <div className="flex flex-col bg-slate-500 text-center h-screen">
+          <h1 className="sticky">List Servers</h1>
+          <nav className="relative flex-grow overflow-y-auto">
+            <ul className="py-6">
+              <li>
+
+              </li>
+            </ul>
+          </nav>
+          <h1 className="sticky p-4">User 1</h1>
+        </div>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
