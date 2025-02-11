ba4dd104b40063ab75e4a138462ac5ccdfb70e25
valentimarco <gamespy67@gmail.com>
2025-02-11 02:00:29 +0100

    feat(frontend): add rule to linter Much better than before...

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  files: ['**/*.vue'],
+  rules: {
+    "vue/first-attribute-linebreak": ["error", {
+      "singleline": "beside",
+      "multiline": "beside"
+    }]
+  }
+})
